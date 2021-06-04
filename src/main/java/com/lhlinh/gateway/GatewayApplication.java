c73a0c73c3f3fa81e0a700b2af82bb5bda390310
Đồng Đạo Hiếu - CMC Global DU11 <ddhieu1@ldap.cmcglobal.com.vn>
2021-06-04 17:04:55 +0700

    first commit

---
+++ src/main/java/com/lhlinh/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.lhlinh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+}
